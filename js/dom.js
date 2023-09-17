ed25f13f409599266da711555ae6871ac779b2df
Avishi Vijay <avishivijay2324@gmail.com>
2023-09-17 23:30:11 +0530

    new

--- js/dom.js
+++ js/dom.js
@@ -7,4 +7,9 @@ console.log(randomnumber1, randomnumber2);
 const image1 = "../image/d" + randomnumber1 +".jpg";
 const image2 = "../image/d" + randomnumber2 +".jpg";
 
+const randomnumber1 = Math.trunc(Math.random() * 6 + 1);
+const randomnumber2 = Math.trunc(Math.random() * 6 + 1);
+console.log(randomnumber1, randomnumber2);
 
+const image1 = "../image/d" + randomnumber1 +".jpg";
+const image2 = "../image/d" + randomnumber2 +".jpg";
